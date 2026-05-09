551f3bacf7f30295de1cc4c9989bc2415e9ef98d
aonami495 <aonami0495@gmail.com>
2026-05-09 17:17:00 +0900

    WIP: before renaming projects to topics

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_05_07_045331) do
+ActiveRecord::Schema[7.2].define(version: 2026_05_07_024402) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "projects", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "name", null: false
-    t.text "description"
-    t.string "emoji"
-    t.boolean "archived", default: false
-    t.integer "position"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_projects_on_user_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,13 +20,11 @@ ActiveRecord::Schema[7.2].define(version: 2026_05_07_045331) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name", null: false
-    t.string "username", null: false
+    t.string "name"
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_foreign_key "projects", "users"
 end
